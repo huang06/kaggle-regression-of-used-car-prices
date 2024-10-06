145c264a7a8b67ee81b7c21f8b4ba808e3781031
huang06 <tom4996131207@gmail.com>
2024-10-06 08:31:28 +0000

    chore: update

--- create-submission.sh
+++ create-submission.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-.venv/bin/python3 -m jupyter nbconvert ./oof_price_bin.ipynb --to script
+python3 -m jupyter nbconvert ./oof_price_bin.ipynb --to script
 time python3 oof_price_bin.py
